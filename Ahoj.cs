d1e0683b4965467d403767de5ed54e9e7d75db76
OPacner <84729405+OPacner@users.noreply.github.com>
2021-10-05 08:32:20 +0200

    Initial commit

---
+++ Ahoj.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uvod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Hello World!!";
+
+            Console.WriteLine(msg);
+        }
+    }
+}
